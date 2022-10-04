f73b0c71926502454754a6c218816b02d7fe3bfb
joak3468 <joaquinmartinez8@msn.com>
2022-10-04 10:19:54 -0300

    login primer paso/falta configurar la base de datos

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,35 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 Route::get( '/', 'HomeController@index' )->name( 'home' );
 Route::get( 'presupuesto_online', 'HomeController@form' )->name( 'form' );
 Route::post( 'send_presupuesto', 'HomeController@send_presupuesto' )->name( 'presupuesto_online' );
 Route::get('/clear_all', function() {
-    Artisan::call('cache:clear');
+Artisan::call('cache:clear');
 Artisan::call('config:cache');
 Artisan::call('view:clear');
 return "Cleared!";
+});
+
+Route::prefix('admin')->group( function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+    
+    require __DIR__.'/auth.php';
 });
